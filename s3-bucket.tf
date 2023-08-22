577e299fff3611751fc76d50ef5b8f1765b05460
gpharris <gpharris@amazon.com>
2023-08-22 20:53:29 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket_prefix = gpharris
-  bucket = "gaurav-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/gpharris-aws/s3-bucket/aws"
+  version = "2.8.0"
 }
